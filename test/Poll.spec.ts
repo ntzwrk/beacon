7538fd0d85147d0ac31e1c8bd97012944c9a6001
Valentin Sundermann <me@vsund.de>
2017-07-14 14:36:57 +0200

    Add more asserts to isRunning()
    as the method without parameters isn't tested yet.

--- test/Poll.spec.ts
+++ test/Poll.spec.ts
@@ -13,7 +13,9 @@ describe("Poll", () => {
 	it("decides correctly whether the poll is running or not", () => {
 		// examplePoll started 2017-07-12 00:00 and ended 24h later
 
+		chai.assert.isFalse(examplePoll.isRunning());
 		chai.assert.isFalse(examplePoll.isRunning(examplePoll.startDate - 1));
+		chai.assert.isTrue(examplePoll.isRunning(examplePoll.startDate));
 		chai.assert.isTrue(examplePoll.isRunning(examplePoll.startDate + (1000*60*60*24)));
 		chai.assert.isFalse(examplePoll.isRunning(examplePoll.startDate + (1000*60*60*24) + 1));
 		chai.assert.isTrue(examplePoll.isRunning(examplePoll.endDate));
