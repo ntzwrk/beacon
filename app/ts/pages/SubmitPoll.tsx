ca675357a0af7a8356df57e57ff2ced3987d2ebe
Valentin Sundermann <me@vsund.de>
2017-07-21 15:39:51 +0200

    Add basic component for submitting polls (skeleton)

---
+++ app/ts/pages/SubmitPoll.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import {Link} from "react-router-dom";
+
+export class SubmitPoll extends React.Component<any, any> {
+	render() {
+		return (
+			<div>
+				<h1>SubmitPoll</h1>
+
+				<pre>@todo</pre>
+				<Link to=".." className="btn btn-default">Back</Link>
+			</div>
+		);
+	}
+}
